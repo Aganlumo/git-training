d9bb32953a9f14d4df3d69ed9b7162a112036c5a
Aganlumey <Agustin.Garcia@mx.ey.com>
2023-11-24 15:18:23 -0600

    Adding shell examples

---
+++ Workflow/CommitExamples.sh
@@ -0,0 +1,11 @@
+# Simple command
+# This will open the editor that you have by default on your terminal
+git commit
+
+# This avoids using the editor
+git commit -m "This is the message from the commit"
+
+# Used when something has changed and you don't want to create another commit
+# This can happend when the changes are typos or you want to change the commit message}
+# It will open the editor that you have by deault on your terminal
+git commit --amend
---
+++ Workflow/StagingExamples.sh
@@ -0,0 +1,38 @@
+# Adding to the staging area
+
+# This will add EVERYTHING that is not ignored in the .gitignore file
+git add *
+
+# Adding files over a specific path
+# This will add all the files on the specific path
+git add path/to/add/
+git add path/to/add/* 
+
+# Adding a specific file
+git add path/to/file.file
+
+# Adding multiple files
+git add file1 file2 file3
+
+# Adding all files with a specific extension on a specific folder
+git add imgs/*.png
+
+# Adding files with a prefix 
+# (useful for when you have procedurally generated configurations or something of the like)
+git add report_*.csv
+
+# Dry run - does not actually add the files to the staging
+git add . --dry-run
+git add -n .
+
+# Override the .gitignore file and add it anyway
+git add my_log.log --force
+
+
+# Removing from the staging area
+git rm file
+
+# Removing all from a directory
+git rm -r dir/*
+
+
